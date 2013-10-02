fd2cb0f27bb3b0a9ec4372fac7f1515a1c80f114
BruSD <dmitry.bruso@mobilesoft365.com>
2013-10-03 00:26:31 +0300

    -Base Calendar

--- src/brusd/mediummg/StudentCalendar/MonthActivity.java
+++ src/brusd/mediummg/StudentCalendar/MonthActivity.java
@@ -10,6 +10,7 @@ public class MonthActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main);
     }
 }
